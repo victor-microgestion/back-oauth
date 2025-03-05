89dc557061ac10ba83ce351bb4c8d3d7a594d02c
victor-microgestion <victor_villafane@microgestion.com>
2025-03-05 11:30:37 -0300

    Cambio en el provider de mongo para que sea singleton y evitar conexiones innecesarias

--- src/main/java/io/openliberty/guides/providers/MongoClientProducer.java
+++ src/main/java/io/openliberty/guides/providers/MongoClientProducer.java
@@ -1,41 +1,51 @@
 package io.openliberty.guides.providers;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Singleton;
 
 @ApplicationScoped
 public class MongoClientProducer {
 
-    String connectionString = "mongodb+srv://admin:admin@java-login.dv96h.mongodb.net/?retryWrites=true&w=majority&appName=java-login";
+    private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
 
-    //@ConfigProperty(name = "User")
-    String dbName = "User";
+    @ConfigProperty(name = "mongo.connectionString")
+    String connectionString;
 
-    @Produces
-    public MongoClient createMongoClient() {
-        return MongoClients.create(
+    @ConfigProperty(name = "mongo.database")
+    String dbName;
+
+    public MongoClientProducer() {
+        this.mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString(connectionString))
+                        .applyConnectionString(new ConnectionString("mongodb+srv://admin:admin@java-login.dv96h.mongodb.net/?retryWrites=true&w=majority&appName=java-login"))
                         .build()
         );
+        this.mongoDatabase = mongoClient.getDatabase("User");
     }
 
     @Produces
-    public MongoDatabase createDatabase(MongoClient mongoClient) {
-        return mongoClient.getDatabase(dbName);
+    @Singleton
+    public MongoClient getMongoClient() {
+        return mongoClient;
     }
 
-    public void closeMongoClient(@Disposes MongoClient mongoClient) {
+    @Produces
+    @Singleton
+    public MongoDatabase getDatabase() {
+        return mongoDatabase;
+    }
+
+    @PreDestroy
+    public void closeMongoClient() {
         mongoClient.close();
     }
 }
